098e033831f3a4d76b53013021b9268bd5e8c3cc
devhtetminmyat <devhtetminmyat@gmail.com>
2026-07-04 18:55:36 +0700

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,25 +11,30 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
-// app.use((req, res, next) => {
-//   console.log(`request method is ${req.method} & request url is ${req.url}`);
-//   next();
-// });
-
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("server started on port:", PORT);
   });
 });
-
-// * mongodb+srv://nekidouglas_db_user:3B4duwGXjNBc5nzP@cluster0.ru3kqau.mongodb.net/?appName=Cluster0
